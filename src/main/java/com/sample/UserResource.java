57880d2479ea4f408faba2a64867bb376e858960
sa-tsunoda <sa-tsunoda@unext.jp>
2023-03-01 13:59:37 +0900

    Add DB access

---
+++ src/main/java/com/sample/UserResource.java
@@ -0,0 +1,37 @@
+package com.sample;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.sample.entity.user.User;
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.mysqlclient.MySQLPool;
+import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.RowIterator;
+import io.vertx.mutiny.sqlclient.Tuple;
+
+@Path("/user")
+public class UserResource {
+    @Inject
+    MySQLPool client;
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<User> hello(@PathParam("id") String id) {
+        return client
+                .preparedQuery("select id,name from user where id = ?")
+                .execute(Tuple.of(id))
+                .onItem()
+                .transform(rows -> {
+                    RowIterator<Row> iterator = rows.iterator();
+                    Row row = iterator.next();
+
+                    return new User(row.getLong("id"),row.getString("name"));
+                });
+    }
+}
---
+++ src/main/java/com/sample/entity/user/User.java
@@ -0,0 +1,13 @@
+package com.sample.entity.user;
+
+public class User {
+
+    public Long id;
+
+    public String name;
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
